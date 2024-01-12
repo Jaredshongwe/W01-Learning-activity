7ff6a28aa7cc8de139dc5fa9f2a04149ddaf9fd0
Jaredshongwe <jaredshongwe@gmail.com>
2024-01-12 11:47:50 +0200

    rectified spelling error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Hello Jared Shogwe");
+  res.send("Hello Jared Shongwe");
 });
  
 const port = 3000
